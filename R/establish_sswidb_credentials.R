5c76a3a70778a4fb269a4bb5191ceab37b0f76a6
jaymwin <jmwiniarski@gmail.com>
2023-08-03 14:29:33 -0500

    importFrom?

--- R/establish_sswidb_credentials.R
+++ R/establish_sswidb_credentials.R
@@ -2,6 +2,7 @@
 #' Title
 #'
 #' @param db_version
+#' @importFrom keyring "key_set"
 #'
 #' @return
 #' @export
@@ -14,20 +15,20 @@ establish_sswidb_credentials <- function(db_version) {
   if (db_version == 'PROD') {
 
     # set dsn
-    keyring::key_set('snapshot_db_dsn')
+    key_set('snapshot_db_dsn')
     # set uid
-    keyring::key_set('snapshot_db_uid')
+    key_set('snapshot_db_uid')
     # set pwd
-    keyring::key_set('snapshot_db_pwd')
+    key_set('snapshot_db_pwd')
 
   } else if (db_version == 'UAT') {
 
     # set dsn
-    keyring::key_set('snapshot_uat_db_dsn')
+    key_set('snapshot_uat_db_dsn')
     # set uid
-    keyring::key_set('snapshot_uat_db_uid')
+    key_set('snapshot_uat_db_uid')
     # set pwd
-    keyring::key_set('snapshot_uat_db_pwd')
+    key_set('snapshot_uat_db_pwd')
 
   } else {
 
