f339c39a416a18b6de02419ac9826af12e8a7931
jaymwin <jmwiniarski@gmail.com>
2023-08-09 12:53:46 -0500

    added suppressWarnings() for overlap function as not necessary to see warnings here

--- R/detect_remove_overlap.R
+++ R/detect_remove_overlap.R
@@ -10,19 +10,21 @@
 
 detect_remove_overlap <- function(effort_df) {
 
-  effort_df <-
-    effort_df %>%
-    # look for overlap at the level of a cam_site_id and year
-    dplyr::group_by(cam_site_id, year) %>%
-    tidyr::nest() %>%
-    # detect_batch_overlap() can take a while
-    # overlap = number of days overlapping between two batches
-    dplyr::mutate(overlap = purrr::map_dbl(data, detect_overlap)) %>%
-    tidyr::unnest(data) %>%
-    dplyr::ungroup() %>%
-    # if 0/-Inf (only one batch or no overlap in batches); retain these
-    dplyr::filter(overlap %in% c(-Inf, 0)) %>%
-    # now discard column
-    dplyr::select(-overlap)
+  suppressWarnings(
+    effort_df <-
+      effort_df %>%
+      # look for overlap at the level of a cam_site_id and year
+      dplyr::group_by(cam_site_id, year) %>%
+      tidyr::nest() %>%
+      # detect_batch_overlap() can take a while
+      # overlap = number of days overlapping between two batches
+      dplyr::mutate(overlap = purrr::map_dbl(data, detect_overlap)) %>%
+      tidyr::unnest(data) %>%
+      dplyr::ungroup() %>%
+      # if 0/-Inf (only one batch or no overlap in batches); retain these
+      dplyr::filter(overlap %in% c(-Inf, 0)) %>%
+      # now discard column
+      dplyr::select(-overlap)
+  )
 
 }
