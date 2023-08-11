041d27c13b7d91352ce7aaaad1f127c1babb8274
jaymwin <jmwiniarski@gmail.com>
2023-08-11 10:28:34 -0500

    fixed raw file writing/project setup

--- R/setup_project.R
+++ R/setup_project.R
@@ -13,9 +13,6 @@ setup_project <- function() {
   # write data folder
   fs::dir_create(here::here('data'))
 
-  # also write subfolder for spatial data
-  fs::dir_create(here::here('data/spatial_data'))
-
   # write output folder
   fs::dir_create(here::here('output'))
 
--- R/write_raw_data.R
+++ R/write_raw_data.R
@@ -14,15 +14,14 @@ write_raw_data <- function(data) {
   # write detections
   data$detections %>%
     # vroom_write() is like write_csv(), but faster
-    # add dates here just to keep track of when data was queried
-    vroom::vroom_write(here::here('data', stringr::str_c('detections_df_raw_', Sys.Date(), '.csv')), delim = ',')
+    vroom::vroom_write(here::here('data/detections_df_raw.csv'), delim = ',')
 
   # write effort
   data$effort %>%
-    vroom::vroom_write(here::here('data', stringr::str_c('effort_df_raw_', Sys.Date(), '.csv')), delim = ',')
+    vroom::vroom_write(here::here('data/effort_df_raw.csv'), delim = ',')
 
   # write locations...need to write as .rds because saving as csv might impact # of coordinate digits
   data$locations %>%
-    readr::write_rds(here::here('data', stringr::str_c('locations_df_raw_', Sys.Date(), '.rds')))
+    readr::write_rds(here::here('data/locations_df_raw.rds'))
 
 }
