de14034b7476d6eb702df8749084176988cd1df8
jaymwin <jmwiniarski@gmail.com>
2023-08-11 14:41:19 -0500

    needed to import functions

--- R/calculate_proportion_classified.R
+++ R/calculate_proportion_classified.R
@@ -18,21 +18,21 @@ calculate_prop_classified <- function(effort, min_date, max_date, min_year, max_
   # needed later when filtering the sswi photo table
   cam_loc_seq_no_x_year <-
     effort %>%
-    distinct(camera_location_seq_no, cam_site_id, year)
+    dplyr::distinct(camera_location_seq_no, cam_site_id, year)
 
   # first, lazily query classifications (i.e., don't load into memory yet)
   class_lazy <-
     conn %>%
     # classification table
-    tbl("SSWI_CLASSIFICATION") %>%
+    dplyr::tbl("SSWI_CLASSIFICATION") %>%
     # get rid of photos with comment status (121; e.g., 'cool' photo comment)
-    filter(METADATA_GROUP_SEQ_NO != 121) %>%
+    dplyr::filter(METADATA_GROUP_SEQ_NO != 121) %>%
     # get rid of zooniverse no consensus (48 AND Z)
-    filter(METADATA_GROUP_SEQ_NO != 48 | USER_GROUP_CODE != 'Z') %>%
+    dplyr::filter(METADATA_GROUP_SEQ_NO != 48 | USER_GROUP_CODE != 'Z') %>%
     # only need one classification per trigger
-    distinct(TRIGGER_SEQ_NO) %>%
+    dplyr::distinct(TRIGGER_SEQ_NO) %>%
     # used to tally 'classified' or not later
-    mutate(classified = 'yes')
+    dplyr::mutate(classified = 'yes')
 
   # if seasons span years, add 1 below to end_date when filtering photo table; otherwise == 0
   year_plus_one <- add_year()
@@ -44,66 +44,66 @@ calculate_prop_classified <- function(effort, min_date, max_date, min_year, max_
   photo_df <-
     conn %>%
     # photo table
-    tbl("SSWI_PHOTO") %>%
+    dplyr::tbl("SSWI_PHOTO") %>%
     # only want distinct triggers
-    distinct(TRIGGER_SEQ_NO, UPLOAD_BATCH_SEQ_NO, CAMERA_LOCATION_SEQ_NO, FINAL_DATE_TIME, PHOTO_TYPE_CODE, DELETE_TRIGGER) %>%
+    dplyr::distinct(TRIGGER_SEQ_NO, UPLOAD_BATCH_SEQ_NO, CAMERA_LOCATION_SEQ_NO, FINAL_DATE_TIME, PHOTO_TYPE_CODE, DELETE_TRIGGER) %>%
     # only interested in motion triggers that shouldn't be deleted
     # and between min/max year
-    filter(PHOTO_TYPE_CODE == 'MOTION_TRIGGERED' & DELETE_TRIGGER == 'NO' & year(FINAL_DATE_TIME) >= min_year & year(FINAL_DATE_TIME) <= max_year) %>%
+    dplyr::filter(PHOTO_TYPE_CODE == 'MOTION_TRIGGERED' & DELETE_TRIGGER == 'NO' & lubridate::year(FINAL_DATE_TIME) >= min_year & lubridate::year(FINAL_DATE_TIME) <= max_year) %>%
     # filter photos within our season date ranges
-    filter(
+    dplyr::filter(
       # this filters season dates for each year
       FINAL_DATE_TIME >= to_date(paste(year(FINAL_DATE_TIME), min_date, sep = ''), "YYYY-MM-DD"),
       # to have inclusive end date add 1 here (and add year if season spans years)
       FINAL_DATE_TIME < to_date(paste(year(FINAL_DATE_TIME) + year_plus_one, max_date, sep = ''), "YYYY-MM-DD") + 1
     ) %>%
     # join classifications to triggers
-    left_join(., class_lazy, by = 'TRIGGER_SEQ_NO') %>%
+    dplyr::left_join(., class_lazy, by = 'TRIGGER_SEQ_NO') %>%
     # clean up
-    select(CAMERA_LOCATION_SEQ_NO, FINAL_DATE_TIME, TRIGGER_SEQ_NO, classified) %>%
+    dplyr::select(CAMERA_LOCATION_SEQ_NO, FINAL_DATE_TIME, TRIGGER_SEQ_NO, classified) %>%
     # convert datetime to date (this actually is still a datetime, but any given image on a day gets
     # a date with YYYY-MM-DD 00:00:00). this allows tallying photos by 'date'
-    mutate(date = to_date(paste(year(FINAL_DATE_TIME), month(FINAL_DATE_TIME), day(FINAL_DATE_TIME), sep = "-"), "YYYY-MM-DD")) %>%
+    dplyr::mutate(date = to_date(paste(lubridate::year(FINAL_DATE_TIME), lubridate::month(FINAL_DATE_TIME), lubridate::day(FINAL_DATE_TIME), sep = "-"), "YYYY-MM-DD")) %>%
     # tally triggers classified or not (== NA) per camera_location_seq_no and date
-    group_by(CAMERA_LOCATION_SEQ_NO, date, classified) %>%
+    dplyr::group_by(CAMERA_LOCATION_SEQ_NO, date, classified) %>%
     # this creates n, which is equal to number of triggers
-    tally() %>%
-    ungroup() %>%
+    dplyr::tally() %>%
+    dplyr::ungroup() %>%
     # organize
-    arrange(CAMERA_LOCATION_SEQ_NO, date, classified) %>%
+    dplyr::arrange(CAMERA_LOCATION_SEQ_NO, date, classified) %>%
     # read data into memory now
-    collect() %>%
+    dplyr::collect() %>%
     # fix date, add year
-    mutate(
+    dplyr::mutate(
       date = as.Date(date),
-      year = year(date)
+      year = lubridate::year(date)
     ) %>%
     # rename to join together to detections/effort/locs dfs later
-    rename(camera_location_seq_no = CAMERA_LOCATION_SEQ_NO)
+    dplyr::rename(camera_location_seq_no = CAMERA_LOCATION_SEQ_NO)
 
   # then join in cam_site_ids as we want to calculate prop. classified at a site-level
   # not by camera_location_seq_no
   photos_by_cam_df <-
     photo_df %>%
     # only keep focal cam sites/cam location seq nos/years with inner_join()
-    inner_join(., cam_loc_seq_no_x_year, by = c("camera_location_seq_no", "year")) %>%
-    select(cam_site_id, camera_location_seq_no, year, date, classified, n) %>%
-    arrange(year, cam_site_id, date, classified) %>%
+    dplyr::inner_join(., cam_loc_seq_no_x_year, by = c("camera_location_seq_no", "year")) %>%
+    dplyr::select(cam_site_id, camera_location_seq_no, year, date, classified, n) %>%
+    dplyr::arrange(year, cam_site_id, date, classified) %>%
 
     # for clarity, classified == NA means photos haven't been classified
     # so just change these NAs to 'no' now
-    mutate(classified = replace_na(classified, 'no')) %>%
+    dplyr::mutate(classified = tidyr::replace_na(classified, 'no')) %>%
 
     # now we need to count classified/unclassified photos by cam_site_id instead of camera_location_seq_no
     # this is because there can be multiple camera_location_seq_nos in the same location in a season
-    group_by(cam_site_id, year, date, classified) %>%
+    dplyr::group_by(cam_site_id, year, date, classified) %>%
     # total photos at a cam_site_id per date
-    summarise(n = sum(n)) %>%
-    ungroup() %>%
+    dplyr::summarise(n = sum(n)) %>%
+    dplyr::ungroup() %>%
 
     # now create columns of counts of unclassified (classified_no) and classified (classified_yes) triggers
     # this helps for calculating proportions below
-    pivot_wider(
+    tidyr::pivot_wider(
       names_from = c(classified),
       names_glue = "{'classified'}_{classified}",
       values_from = n,
@@ -113,40 +113,40 @@ calculate_prop_classified <- function(effort, min_date, max_date, min_year, max_
 
     # now tally the total number of photos per cam site and date by adding
     # the number of unclassified and classified triggers
-    mutate(
+    dplyr::mutate(
       total = classified_no + classified_yes
     ) %>%
     # rename classified_yes to classified; just need this and total photos to get proportion
-    select(cam_site_id, year, date, classified = classified_yes, total) %>%
-    arrange(cam_site_id, year, date) %>%
+    dplyr::select(cam_site_id, year, date, classified = classified_yes, total) %>%
+    dplyr::arrange(cam_site_id, year, date) %>%
 
     # add in day of season, occasion period for each date so the proportion classified (joined by date in photos)
     # can be calculated over a cam_site_id and sampling occasion
 
     # assign day to equal interval bin
-    left_join(., day_occasion_df, by = c("year", "date")) %>%
+    dplyr::left_join(., day_occasion_df, by = c("year", "date")) %>%
 
     # now sum photos over sampling occasion for each camera
     # organize
-    arrange(cam_site_id, year, date) %>%
-    group_by(cam_site_id, year, occ) %>%
-    summarise(
+    dplyr::arrange(cam_site_id, year, date) %>%
+    dplyr::group_by(cam_site_id, year, occ) %>%
+    dplyr::summarise(
       # number classified in an occasion
       classified = sum(classified),
       # total photos in an occasion
       total = sum(total)
     ) %>%
-    ungroup() %>%
+    dplyr::ungroup() %>%
 
     # at this point, there will be missing occasions if there are no photos over certain dates
     # zero-fill in any missing occasions; classified and total == 0 for missing data
-    group_by(cam_site_id, year) %>%
-    complete(occ = 1:num_occasions, fill = list(classified = 0, total = 0)) %>%
-    ungroup()
+    dplyr::group_by(cam_site_id, year) %>%
+    tidyr::complete(occ = 1:num_occasions, fill = list(classified = 0, total = 0)) %>%
+    dplyr::ungroup()
 
   effort_ppn_df <-
     effort %>%
-    left_join(
+    dplyr::left_join(
       .,
       photos_by_cam_df,
       by = c("cam_site_id", "year", "occ")
@@ -155,12 +155,12 @@ calculate_prop_classified <- function(effort, min_date, max_date, min_year, max_
     # there are some NAs now...seem to be when there is effort but no photos in the photo table
     # or 0 effort (and thus no photos)
     # so turn those into 0s now
-    mutate(
-      classified = replace_na(classified, 0),
-      total = replace_na(total, 0)
+    dplyr::mutate(
+      classified = tidyr::replace_na(classified, 0),
+      total = tidyr::replace_na(total, 0)
     ) %>%
     # calculate proportion classified
-    mutate(
+    dplyr::mutate(
       ppn_classified = case_when(
         # I think we want ppn to be 1 here as there are no photos to be classified?
         total == 0 ~ 1,
--- R/create_sampling_occasions.R
+++ R/create_sampling_occasions.R
@@ -15,48 +15,48 @@ create_sampling_occasions <- function(seasons, effort, num_occasions) {
   # create day of season data frame to join occasion number to effort/detection dates
   day_occasion_df <-
     seasons %>%
-    group_by(year) %>%
-    nest() %>%
+    dplyr::group_by(year) %>%
+    tidyr::nest() %>%
     # create date sequence for each year
-    mutate(date = map(data, date_sequence)) %>%
-    unnest(date) %>%
-    select(-data) %>%
+    dplyr::mutate(date = purrr::map(data, date_sequence)) %>%
+    tidyr::unnest(date) %>%
+    dplyr::select(-data) %>%
     # using row_number give day of season starting with day 1
-    mutate(day_of_season = row_number()) %>%
-    ungroup() %>%
+    dplyr::mutate(day_of_season = dplyr::row_number()) %>%
+    dplyr::ungroup() %>%
     # split season into equal intervals (1-day, 3-day, ...1 week)
     # ntile() assigns each day into a sampling occasion
-    mutate(
-      occ = ntile(day_of_season, num_occasions)
+    dplyr::mutate(
+      occ = dplyr::ntile(day_of_season, num_occasions)
     )
 
   # now summarize effort over sampling occasion
   effort <-
     effort %>%
-    left_join(
+    dplyr::left_join(
       .,
       # rename date as date_active to match effort data frame
-      day_occasion_df %>% select(date_active = date, day_of_season, occ),
+      day_occasion_df %>% dplyr::select(date_active = date, day_of_season, occ),
       by = "date_active"
     ) %>%
     # remove any overlapping dates (i.e., when camera_location_seq_no effort ends/starts at same site)
-    distinct(year, cam_site_id, date_active, day_of_season, occ) %>%
-    group_by(year, cam_site_id, occ) %>%
+    dplyr::distinct(year, cam_site_id, date_active, day_of_season, occ) %>%
+    dplyr::group_by(year, cam_site_id, occ) %>%
     # calculate number of days per occasion by using add_count()
-    add_count() %>%
-    summarise(
+    dplyr::add_count() %>%
+    dplyr::summarise(
       # days of effort for each cam_site x occ x year = max of n
       days_active = max(n)
     ) %>%
-    ungroup()
+    dplyr::ungroup()
 
   # zero-fill in effort for any missing occasions here
   # ONLY for cam_site_id x year combinations where a camera is already operational
   effort <-
     effort %>%
-    group_by(cam_site_id, year) %>%
-    complete(occ = 1:num_occasions, fill = list(days_active = 0)) %>%
-    ungroup()
+    dplyr::group_by(cam_site_id, year) %>%
+    tidyr::complete(occ = 1:num_occasions, fill = list(days_active = 0)) %>%
+    dplyr::ungroup()
 
   return(effort)
 
