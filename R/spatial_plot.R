4ee445fc221a5438fb0dee9402f2ebc1434d09a0
Wildey <wildeefb@DNR.state.wi.us>
2025-08-14 16:41:14 -0500

    Fixed bug in spatial_plot with titles showing up too large in rmarkdowns

--- R/spatial_plot.R
+++ R/spatial_plot.R
@@ -90,12 +90,7 @@ spatial_plot <- function (conn, df, mgmtlayer="counties", days_active_threshold,
       geom_sf(color="white", mapping=aes(fill=mean)) +
       #geom_sf(data=mgmtlayer, color="white", lwd=1,fill=NA) +
       labs(title=stringr::str_wrap(sprintf("%s -- Proportion of Snapshot Cameras with Detection", titles[j]),80)) +
-      scale_fill_continuous() +
-      theme(legend.title = element_blank(),
-            legend.text=element_text(size=16),
-            plot.title = element_text(size=22,hjust = 0.5),
-            axis.text.x = element_text( size = 16),
-            axis.text.y = element_text( size = 16)))
+      scale_fill_continuous())
 
   names(plotlist) <- specieslist
   return(plotlist)
